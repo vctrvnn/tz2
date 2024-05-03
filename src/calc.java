a9845d96672d7e7c7216da46be5f4124a4ffae02
Victor Vanin <vaninvic05@yandex.ru>
2024-05-03 18:18:16 +0300

    uprate

--- src/calc.java
+++ src/calc.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class calc {
     public static void main(String[] args) {
-        // Укажите путь к вашему файлу
         String filePath = "numbers.txt";
         
         try {
